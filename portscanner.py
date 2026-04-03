36b388209495519a0eda7a6d29c8d8b262803cbd
Eiyarrr <eiyar@eiyar.com>
2026-04-03 20:01:36 +0000

    very basic socket functionality

--- portscanner.py
+++ portscanner.py
@@ -1,16 +1,21 @@
 import sys
+import socket
+
+def is_open(port):
+    #check if open
+    return port
+
 
 if len(sys.argv) != 2:
     print("Invalid number of arguments")
     sys.exit(-1)
 
-target = sys.argv[1]
-print("Target: " + target)
+target_hostname = sys.argv[1]
+target_ip = socket.gethostbyname(target_hostname)
+print("Target Hostname  " + target_hostname)
+print("Target IP        " + target_ip)
 
 for port in range(1,65535):
     if is_open(port):
         print("Port {} is open")
 
-def is_open(port):
-    return true
-    #check if open
