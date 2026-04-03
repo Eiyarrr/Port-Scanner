bc032d271ea75d95e195bcbafa403724fd7527c3
Eiyarrr <eiyar@eiyar.com>
2026-04-03 20:08:14 +0000

    added tracking of number of open ports

--- portscanner.py
+++ portscanner.py
@@ -6,7 +6,7 @@ def is_open(port):
     return port
 
 
-if len(sys.argv) != 2:
+if len(sys.argv) is not 2:
     print("Invalid number of arguments")
     sys.exit(-1)
 
@@ -15,7 +15,10 @@ target_ip = socket.gethostbyname(target_hostname)
 print("Target Hostname  " + target_hostname)
 print("Target IP        " + target_ip)
 
+total_open = 0
 for port in range(1,65535):
     if is_open(port):
         print("Port {} is open")
+        total_open += 1
 
+print("Total open ports: " + str(total_open))
