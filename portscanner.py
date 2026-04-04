605ec804cbe7ca2775f4ee69e74b8e2d7d8abc20
Eiyarrr <eiyar@eiyar.com>
2026-04-04 00:31:18 +0000

    Added basic error handling in is_open

--- portscanner.py
+++ portscanner.py
@@ -2,10 +2,20 @@ import sys
 import socket
 
 def is_open(target, port):
-    print("is_open" + str(target) + str(port))
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(1)
-    result = s.connect_ex((target, port))
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(1)
+        result = s.connect_ex((target, port))
+    except KeyboardInterrupt:
+        print(f"KeyboardInterrupt on port {port}! Exiting program!")
+        sys.exit(0)
+    except socket.gaierror:
+        print(f"socket.gaierror on port {port}! Skipping port!")
+        return False
+    except socket.error:
+        print(f"socket.error on port {port}! Skipping port!")
+        return False
+
     s.close()
     if result == 0:
         return True
@@ -22,7 +32,7 @@ print("Target Hostname  " + target_hostname)
 print("Target IP        " + target_ip)
 
 total_open = 0
-for port in range(1,6):
+for port in range(1,65535):
     if is_open(target_ip, port):
         print(f"Port {port} is open")
         total_open += 1
