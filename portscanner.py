e0f40cd20e307c66020e3d59f672f3494f874a35
Eiyarrr <eiyar@eiyar.com>
2026-04-04 00:10:33 +0000

    added basic is_open functionality

--- portscanner.py
+++ portscanner.py
@@ -1,12 +1,18 @@
 import sys
 import socket
 
-def is_open(port):
-    #check if open
-    return port
+def is_open(target, port):
+    print("is_open" + str(target) + str(port))
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(1)
+    result = s.connect_ex((target, port))
+    s.close()
+    if result == 0:
+        return True
+    return False
 
 
-if len(sys.argv) is not 2:
+if len(sys.argv) != 2:
     print("Invalid number of arguments")
     sys.exit(-1)
 
@@ -16,9 +22,9 @@ print("Target Hostname  " + target_hostname)
 print("Target IP        " + target_ip)
 
 total_open = 0
-for port in range(1,65535):
-    if is_open(port):
-        print("Port {} is open")
+for port in range(1,6):
+    if is_open(target_ip, port):
+        print(f"Port {port} is open")
         total_open += 1
 
-print("Total open ports: " + str(total_open))
+print(f"Total open ports: {total_open}")
