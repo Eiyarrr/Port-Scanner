a9d3f2c3752c9550fab54cbd2489fc1eb9d2989f
Eiyarrr <eiyar@eiyar.com>
2026-04-04 03:27:37 +0000

    Added basic async functionality

--- portscanner.py
+++ portscanner.py
@@ -1,40 +1,29 @@
 import sys
 import socket
+import asyncio
 
-def is_open(target, port):
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.settimeout(1)
-        result = s.connect_ex((target, port))
-    except KeyboardInterrupt:
-        print(f"KeyboardInterrupt on port {port}! Exiting program!")
-        sys.exit(0)
-    except socket.gaierror:
-        print(f"socket.gaierror on port {port}! Skipping port!")
-        return False
-    except socket.error:
-        print(f"socket.error on port {port}! Skipping port!")
-        return False
+async def is_open(target, port):
+    _, writer = await asyncio.open_connection(target, port)
+    print(f"Port {port} is open")
+    writer.close()
+    await writer.wait_closed()
+    return True
 
-    s.close()
-    if result == 0:
-        return True
-    return False
+async def main():
+    if len(sys.argv) != 2:
+        print("Invalid number of arguments")
+        sys.exit(-1)
 
+    hostname = sys.argv[1]
+    target = socket.gethostbyname(hostname)
+    print("Target Hostname  " + hostname)
+    print("Target IP        " + target)
 
-if len(sys.argv) != 2:
-    print("Invalid number of arguments")
-    sys.exit(-1)
+    all_ports = [is_open(target, port) for port in range(1, 65535)]
+    results = await asyncio.gather(*all_ports)
+    
+    total_open = sum(results)
 
-target_hostname = sys.argv[1]
-target_ip = socket.gethostbyname(target_hostname)
-print("Target Hostname  " + target_hostname)
-print("Target IP        " + target_ip)
+    print(f"Total open ports: {total_open}")
 
-total_open = 0
-for port in range(1,65535):
-    if is_open(target_ip, port):
-        print(f"Port {port} is open")
-        total_open += 1
-
-print(f"Total open ports: {total_open}")
+asyncio.run(main())
