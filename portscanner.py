fb0734298bee49c11c14a7b6428897188d62ce17
Eiyarrr <eiyar@eiyar.com>
2026-04-04 03:44:28 +0000

    Added exception catching to make functional, removed unnecessary inclusion of socket, increased semaphone limit to 1000

--- portscanner.py
+++ portscanner.py
@@ -1,13 +1,20 @@
 import sys
-import socket
 import asyncio
 
+semaphore = asyncio.Semaphore(1000)
+
 async def is_open(target, port):
-    _, writer = await asyncio.wait_for(asyncio.open_connection(target, port), timeout = 1)
-    print(f"Port {port} is open")
-    writer.close()
-    await writer.wait_closed()
-    return True
+    async with semaphore:
+        try:
+            _, writer = await asyncio.wait_for(asyncio.open_connection(target, port), timeout = 1)
+            print(f"Port {port} is open")
+            writer.close()
+            await writer.wait_closed()
+            return True
+        except asyncio.TimeoutError:
+            return False
+        except OSError:
+            return False
 
 async def main():
     if len(sys.argv) != 2:
